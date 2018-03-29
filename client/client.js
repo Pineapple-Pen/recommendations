ff89820c5474f235e2f75d8d4da7c8c8439b57df
Eric Sandoval <ericmsandoval@gmail.com>
2018-03-29 13:03:47 -0700

    Bug fix for double rendering on proxy; currently renders correctly on module server and proxy server

--- client/client.js
+++ client/client.js
@@ -1,6 +1,3 @@
-import React from 'react'
 import Recommendations from './src/index.jsx';
-import ReactDOM from 'react-dom';
 
 window.Recommendations = Recommendations;
-ReactDOM.render(<Recommendations />, document.getElementById('Recommendations') );
